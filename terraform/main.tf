64d7fe10a97be2cf6f56eb2512061f0dce26e1d9
houssamjdya <houssam.jdya@outlook.com>
2026-03-05 20:18:42 +0100

    feat: add azure key vault integration

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,3 +62,38 @@ resource "azurerm_role_assignment" "aks_acr_pull" {
   scope                            = azurerm_container_registry.main.id
   skip_service_principal_aad_check = true
 }
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "main" {
+  name                = "kv-microservices-plat"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    secret_permissions = [
+      "Get", "List", "Set", "Delete"
+    ]
+  }
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = azurerm_kubernetes_cluster.main.kubelet_identity[0].object_id
+
+    secret_permissions = [
+      "Get", "List"
+    ]
+  }
+}
+
+# Eksempel-hemmelighet
+resource "azurerm_key_vault_secret" "db_password" {
+  name         = "db-password"
+  value        = "super-secret-password-123"
+  key_vault_id = azurerm_key_vault.main.id
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,3 +12,7 @@ output "resource_group_name" {
     description = " Resource group name"
     value = azurerm_resource_group.main.name
 }
+
+output "key_vault_name" {
+    value = azurerm_key_vault_main.name
+}
